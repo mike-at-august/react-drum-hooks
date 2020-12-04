e33d3410219a72b49312b6ffe01a1cad50f91a48
Mike Vardy <mike@alwaysaugust.co>
2020-12-04 08:19:56 -0500

    convert display to functional

--- src/componets/Display.js
+++ src/componets/Display.js
@@ -1,29 +1,27 @@
 import React from "react";
 import Volume from "./Volume";
 
-class Display extends React.Component {
-  render() {
-    return (
-      <div id="display">
-        <div className="power">
-          <p>Power</p>
-          <button className="switchPower" onClick={this.props.switchPower}>
-            <div className="square"></div>
-          </button>
-        </div>
-        <div className="sounds-names">
-          <p></p>
-        </div>
-        <Volume power={this.props.power} />
-        <div className="bank">
-          <p>Bank</p>
-          <button onClick={this.props.changeBank}>
-            <div className="square"></div>
-          </button>
-        </div>
+const Display = ({ changeBank, switchPower, power }) => {
+  return (
+    <div id="display">
+      <div className="power">
+        <p>Power</p>
+        <button className="switchPower" onClick={switchPower}>
+          <div className="square"></div>
+        </button>
       </div>
-    );
-  }
-}
+      <div className="sounds-names">
+        <p></p>
+      </div>
+      <Volume power={power} />
+      <div className="bank">
+        <p>Bank</p>
+        <button onClick={changeBank}>
+          <div className="square"></div>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Display;
